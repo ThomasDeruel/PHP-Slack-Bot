a8341284894835c777eef2609b2781d0007942af
Thomas Deruel <t.deruel@hotmail.fr>
2019-01-18 16:02:54 +0100

    starter

--- public/bot.php
+++ public/bot.php
@@ -12,12 +12,9 @@ DriverManager::loadDriver(SlackRTMDriver::class);
 $loop = Factory::create();
 $botman = BotManFactory::createForRTM([
     'slack' => [
-        'token' => 'JETON-D-API-DE-VOTRE-BOT',
+        'token' => 'xoxb-527238373074-527075155572-4I1Fl9oHAC6keYKAv3ImiX2k',
     ],
 ], $loop);
-
-
-
 
 
 // Dès que le bot entends le terme "PHP", il répond:
@@ -25,8 +22,11 @@ $botman->hears('.* PHP .*', function($bot) {
     $bot->reply('le PHP c\'est badass !! 🤘');
 });
 
+$phpBot = new phpBot();
 
+$botman->hears('start', [$phpBot, 'demarrerPartie']);
+$botman->hears('stop', [$phpBot, 'arreterPartie']);
+$botman->hears('statut', [$phpBot, 'obtenirStatut']);
 
-
-
+$botman->hears('lettre (.)', [$phpBot, 'proposerCaractere']);
 $loop->run();
--- src/PhpBot.php
+++ src/PhpBot.php
@@ -5,5 +5,68 @@
 */
 class PhpBot
 {
+    /**
+     * @var bool $statut    Une partie à commencer si true, sinon false
+     * @var string $reponse
+     * @var array $charlist
+     * @var int $chances
+     */
+    private $statut = false;
+    private $reponse;
+    private $charList;
+    private $chance;
 
+    public function demarrerPartie($bot)
+    {
+        $this->statut = true;
+
+        //Choix aléatoire de la fonction à faire deviner
+        $fonctions = get_defined_functions()['internal'];
+        $index = random_int(0, count($fonctions));
+        $this->reponse = $fonctions[$index];
+
+        // Remettre à 0 la liste des caractères
+        $this->charList = [];
+        $this->chances = 10;
+        // Casser la réponse par caractères
+        $characters = str_split($this->reponse);
+        foreach ($characters as $char) {
+            $this->charList[] = [
+                'char'  =>$char,
+                'found' => false,
+            ];
+        }
+        $bot->reply('HuMMMMMM on DEMARRe lAAA PaArRtIee ' . $this->reponse);
+        $bot->reply('Fonction: ' . $this->obtenirMotCle());
+        $bot->reply('Chance:' . $this->chances);
+    }
+    
+    public function obtenirMotCle() : string 
+    {
+        $str = '';
+
+        foreach($this->charList as $char) {
+            if($char['found']){
+                $str .= $char['char'];
+            } else {
+                $str .= '#';
+            }
+        }
+
+        return $str;
+    }
+    public function proposerCaractere($bot, $char) {
+        $bot->reply('Envoi: ' . $char);
+    }
+    
+    public function arreterPartie($bot)
+    {
+        $this->statut = false;
+        $bot->reply('HUUUUUUUUMMMMM Arrêt de la partie ! ');
+    }
+
+    public function obtenirStatut($bot)
+    {
+        $bot->reply('Statut: ' . ($this->statut ? 'on' : 'off'));
+    }
 }
